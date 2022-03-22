d10eaaa8868c95d48e171a538c23dd626a46329d
Matthew Jett <mjj0013@uah.edu>
2022-03-22 15:12:52 -0500

    commit

--- src/wikiSubject.js
+++ src/wikiSubject.js
@@ -1,30 +1,54 @@
-import {React} from 'react';
-import "regenerator-runtime/runtime";
+import React from 'react';
+// import "regenerator-runtime/runtime";
 
 
-//https://en.wikipedia.org/wiki/Special:Random/
+//https://en.wikipedia.org/wiki/Special:Random/api.php?&origin=*
 // See also links: id="mw-content-text" --> class="mw-parser-output" --> <ul>
 
 
 
+//"https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Belgium&limit=5"
 
 
-
-class WikiSubject extends React.Component {
+export class WikiSubject extends React.Component {
     constructor(props) {
         super(props);
 
         // this.data = props.isGenerated?
         this.getNewSubject = this.getNewSubject.bind(this);
         this.branchSubject = this.branchSubject.bind(this);
+
+        
     }
 
+    getNewSubject() {
+        //http://en.wikipedia.org/w/api.php?origin=*&action=query&generator=random&prop=extracts&exchars=500&format=json&callback=onWikipedia
+        // fetch("https://en.wikipedia.org/api.php?&origin=*/wiki/Special:Random/")
+        fetch("http://en.wikipedia.org/w/api.php?origin=*&generator=random&action=query")
+        .then(response => {
+            return response;
+        })
+        .then(data=> {
+            console.log("data",data);
+        })
+        return;
+    }
 
+    branchSubject() {
 
+    }
 
+    
 
     render() {
+        
+        return (
+            <div>
+                {this.getNewSubject()}
+            </div>
 
+
+        )
     }
 
 }
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+	template: __dirname + '/public/index.html',
+	filename: './index.html',
+	inject: false
+});
+const path = require('path');
+module.exports = {
+	entry: './src/index.js',
+	mode: 'development',
+	module: {
+		rules: [
+		   {
+		   	test: /\.(js|jsx)$/,
+		   	exclude: /node_modules/,
+		   	use: {loader: 'babel-loader'},
+		   },
+		   { 
+		   	test: /\.css$/, 
+		   	use: [
+		   		{
+		   		    loader: 'style-loader',
+		   		    options: {esModule: false}
+		   		},
+		   		
+			   	{
+					   loader: 'css-loader',
+				}
+				
+			]
+		   },
+		   {
+			test: /\.(png|jpe?g|gif)$/i,
+			use: [
+			  {
+				loader: 'file-loader',
+				options: {
+					name: 'img/[name].[ext]'
+				}
+			  },
+			],
+			
+		  },
+		  {
+			test: /\.svg$/,
+			use: [
+			  {
+				loader: 'svg-url-loader',
+				options: {limit: 10000,},
+			  },
+			],
+		  },
+		]
+		
+	},
+	output:{
+		filename: 'transformed.js',
+		path: path.resolve(__dirname, 'public'),
+		publicPath: '/',
+		clean: true
+	},
+	devServer: {
+		host: 'localhost',
+		port: 8080,
+		historyApiFallback: true,
+		open: true,
+		hot: true,
+	},
+	
+	plugins: [HTMLWebpackPluginConfig,],
+	resolve: {	
+        extensions: [".js", ".jsx"]
+    },
+	
+	
+};
